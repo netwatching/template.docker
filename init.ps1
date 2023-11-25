2f1413ff29d9e78940eef9c38e1ab7f8d2ae4197
Tobias Steiger <tobias@steigerto.com>
2023-11-25 20:56:19 +0100

    fix: init script

--- init.ps1
+++ init.ps1
@@ -12,14 +12,29 @@ $jsonContent = @"
 "@
 Set-Content -Path ".release-please-manifest.json" -Value $jsonContent
 
-# Clear or create readme.md
-if (Test-Path -Path "readme.md") {
+# Clear or create README.md
+if (Test-Path -Path "README.md") {
     # Clear contents if file exists
-    Set-Content -Path "readme.md" -Value ""
+    Set-Content -Path "README.md" -Value ""
 } else {
     # Create an empty file if it does not exist
-    New-Item -Path "readme.md" -ItemType "file"
+    New-Item -Path "README.md" -ItemType "file"
 }
+
+# Get basic repo information and write to README.md
+$repoInfo = git config --get remote.origin.url
+$repoName = $repoInfo.Split('/')[-1].Replace('.git','')
+$readmeContent = "Repository Name: $repoName`nMore information will be added soon."
+Set-Content -Path "README.md" -Value $readmeContent
+
+# Add changes to staging
+git add .
+
+# Commit changes with a custom message
+git commit -m "chore: release 0.0.1" -m "Release-As: 0.0.1"
+
+# Push changes to the repository
+git push 
 
 # Schedule the deletion of this script (init.ps1) after a short delay
 Start-Sleep -Seconds 1
